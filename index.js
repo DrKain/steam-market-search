28b9d2a0fa188efd50e0b95483b317f9714c56d5
Trekklog <trekklog@gmail.com>
2018-11-27 00:01:21 +0200

    ES6/simplified syntax

--- index.js
+++ index.js
@@ -1,73 +1,59 @@
-var querystring     = require('querystring');
-var https           = require('https');
+const querystring = require('querystring');
+const fetch = require('node-fetch')
 
-function call(url){
-    return new Promise(function(resolve, reject){
-        var items = [];
+const steam_url_market_search = 'https://steamcommunity.com/market/search/render/?'
+const steam_icon_url_route = 'https://steamcommunity-a.akamaihd.net/economy/image/'
+const steam_item_url_route = 'https://steamcommunity.com/market/listings/753/'
 
-        https.get(url, function(res){
-            var body = '';
-            res.on('data', function(chunk){ body += chunk; });
-            res.on('end', function(){
-                var jdata = JSON.parse(body);
-                jdata['results'].map(function(item){
-                    var proc = Object.assign(item, {
-                        appid       : item['asset_description']['appid'],
-                        sell_price  : item.sell_price / 100, // 37 => 0.37
-                        item_type   : discoverItemType(item['asset_description']['type']), // Clearer types
-                        image       : 'https://steamcommunity-a.akamaihd.net/economy/image/' + item['asset_description']['icon_url'],
-                        link        : 'https://steamcommunity.com/market/listings/753/' + encodeURIComponent(item['asset_description']['market_hash_name'])
-                    });
-                    // The following is to clean up the object and remove item-specific data that will change depending on the cheapest listing
-                    delete proc['app_icon']; // Always same
-                    delete proc['app_name']; // Always same
-                    delete proc['sell_price_text']; // Unnecessary
-                    delete proc['asset_description']; // Unnecessary
-                    // Explained below:
-                    function nocall(){
-                        delete proc['asset_description']['type']; // Not clear enough
-                        delete proc['asset_description']['appid']; // Duplicated value
-                        delete proc['asset_description']['classid']; // Unreliable due to change
-                        delete proc['asset_description']['icon_url']; // Not needed with proc.image
-                        delete proc['asset_description']['tradable']; // Unnecessary
-                        delete proc['asset_description']['instanceid']; // Unnecessary
-                        delete proc['asset_description']['name_color']; // Empty values
-                        delete proc['asset_description']['market_hash_name']; // Duplicated value
-                        delete proc['asset_description']['background_color']; // Empty values
-                    }
-                    items.push(proc);
-                });
-                resolve(items);
-            });
-        }).on('error', reject);
+// map -- async f(getMarketItems) > async f(call) > f(pattern_finder)
 
-    });
+module.exports = {
+    async getMarketItems(appid) {
+        // build url parameters
+        const options = querystring.stringify({
+            query: '',
+            start: '0',
+            count: '50', // no game has more than 50 items, multi page is not required
+            search_descriptions: 0,
+            sort_column: 'price',
+            sort_dir: 'desc',
+            appid: '753', // this is the appid for steam, don't change it
+            'category_753_Game[]': 'tag_app_' + appid, // APPID of the items we are searching for
+            norender: 1
+        })
+
+        const market_items = await this.call(`${steam_url_market_search}${options}`)
+
+        return {
+            count: market_items.length,
+            items: market_items
+        }
+    },
+
+    async call(url) {
+        const url_call = await fetch(url)
+        const res = await url_call.json()
+
+        let items = res.results.map(item => {
+            return {
+                name: item.name,
+                hash_name: item.hash_name,
+                sell_price: item.sell_price / 100,
+                sell_price_text: item.sell_price_text,
+                sell_listings: item.sell_listings,
+                appid: item.asset_description.appid,
+                item_type: this.pattern_finder(item.asset_description.type),
+                icon_url: `${steam_icon_url_route}${item.asset_description.icon_url}`,
+                link: `${steam_item_url_route}${encodeURIComponent(item.asset_description.market_hash_name)}`
+            }
+        }, [])
+
+        return items
+    },
+
+
+    pattern_finder(pattern) {
+        const reg = /(Booster Pack|Trading Card|Emoticon|Profile Background)/g
+        return pattern.match(reg)[0]
+    },
 }
-
-function discoverItemType(name){
-    if(name === "Booster Pack") return "Booster Pack";
-    if(name.indexOf("Trading Card") > -1) return "Trading Card";
-    if(name.indexOf("Emoticon") > -1) return "Emoticon";
-    if(name.indexOf("Profile Background") > -1) return "Background";
-    return name;
-}
-
-function getMarketItems(appid){
-    return new Promise(function(resolve, reject){
-        // Build url parameters
-        var options = querystring.stringify({
-            query : '', start : '0',
-            // No game has more than 50 items, Multi page is not required
-            count : '50', search_descriptions : 0,
-            sort_column : 'price', sort_dir : 'desc',
-            // This is the appid for steam, Do no change it
-            appid : '753',
-            // Appid of the items we are searching for
-            'category_753_Game[]' : 'tag_app_' + appid,
-            norender : 1
-        });
-        call('https://steamcommunity.com/market/search/render/?' + options).then(resolve, reject);
-    });
-}
-
-module.exports = getMarketItems;
