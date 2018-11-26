b59e8f35da297d0179a8630923e7499eecaf7f9e
Trekklog <trekklog@gmail.com>
2018-11-27 00:04:14 +0200

    basic test updated

--- tests/basic.js
+++ tests/basic.js
@@ -2,26 +2,11 @@ var SteamMarketSearch = require('../index');
 
 // 311210 = Call of Duty: Black Ops III
 
-SteamMarketSearch("311210").then(function(items){
+SteamMarketSearch.getMarketItems(311210).then(items => {
+    console.log(items.count)
+    // 37
+    console.log(items.items)
 
-    // Print all items found
-    console.log(JSON.stringify(items, null, 2));
-
-    /* Output: {
-        "name": "Stronghold",
-        "hash_name": "311210-Stronghold",
-        "sell_listings": 771,
-        "sell_price": 0.04,
-        "sale_price_text": "$0.03",
-        "appid": 753,
-        "item_type": "Trading Card",
-        "image": "https://steamcommunity-a.akamaihd.net/economy/image/IzMF03bk9WpSBq-S-ekoE33L-iLqGFHVaU25ZzQNQcXdA3g5gMEPvUZZEaiHLrVJRsl8vGuCUY7Cjc9ehDNVzDMFe3OqjCQrcex4NM6b8xHpruyKFHv7bHHNIiDeGQNqHrRdNGiP-Wak4bydQzHAF-4rQ1tWdaYC-20bbMiOPkc-gI8D_Gfh2VRzGVAqfddCdR2Ew3kSNrh4kXdCfJtRzCD3JJyP1A5jPhJoC7jvX7jFbIPwlSx3DhJjTKFIbImVvTuspsDnLPqHZlWIYvE",
-        "link": "https://steamcommunity.com/market/listings/753/311210-Stronghold"
-    } */
-
-
-}, function(err){
-
-    console.log("Error : " + err);
-
-});
+}).catch(err => {
+    console.log(`[ERR] ${err.name} -- ${err.message}`)
+})
